13a73cffec375cb019ce8ab0f178df7a36c4f1d5
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2023-12-20 21:20:58 +0600

    fix output and use FOR for vocabulary

---
+++ for.js
@@ -0,0 +1,23 @@
+// let bok = 'a'
+// let bok = 'b'
+// let bok = 'c'
+
+// let books = ['A', 'B','C']
+
+// for (let i=0; i<books.length; i++){
+//     document.write(books[i], '<br>')
+// }
+
+
+let list = [
+    ['молоко', 'хлеб', 'яйцы'],
+    ['сок', 'мороженное', 'пироженое'],
+    ['корм для кота', 'шымпунь']
+]
+
+for (i = 0; i < list.length; i++) {
+    document.write(list[i])
+    for (j = 0; j < list.length; j++) {
+
+    }
+}
