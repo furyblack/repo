ee4b0e4c0c0474bc55d09e26667009ce85cd0062
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-06-27 21:25:32 +0500

    fix some tasks

--- freeCodeCamp/for decisions.js
+++ freeCodeCamp/for decisions.js
@@ -1,8 +1,24 @@
-// Setup
-const myArray = [];
+// // Setup
+// const myArray = [];
+//
+// // Only change code below this line
+// for(i=1; i<6;i++){
+//     myArray.push(i)
+// }
+// console.log(myArray)
 
-// Only change code below this line
-for(i=1; i<6;i++){
-    myArray.push(i)
-}
-console.log(myArray)
+
+
+// function multiplyAll(arr) {
+//     let product = 1;
+//     // Only change code below this line
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr[i].length; j++) {
+//             product *= arr[i][j];
+//         }
+//     }
+//     // Only change code above this line
+//     return product;
+// }
+//
+// console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]])); // Выводит 5040
